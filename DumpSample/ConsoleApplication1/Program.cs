8bb7cdb1a1f1f6b7f78b5c4f55461da1afecd559
ZhongleiYang <zhleiyang@gmail.com>
2013-07-17 18:25:08 -0700

    add dump sample

---
+++ DumpSample/ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "MiniDumpDemo_Mainline.mdmp");
+
+            using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.ReadWrite, FileShare.Write))
+            {
+                try
+                {
+                    File.Open("1.txt", FileMode.Open);
+                }
+                catch (Exception e)
+                {
+                    MiniDump.Write(fs.SafeFileHandle, MiniDump.Option.WithFullMemory);
+                }
+            }
+
+        }
+    }
+}
---
+++ DumpSample/ConsoleApplication2/Source.cpp
@@ -0,0 +1,109 @@
+﻿#include <windows.h>
+
+#include <Dbghelp.h>
+#include<tchar.h>
+
+using namespace std;
+
+ 
+
+#pragma auto_inline (off)
+
+#pragma comment( lib, "DbgHelp" )
+
+ 
+
+// 为了程序的简洁和集中关注关心的东西，按示例程序的惯例忽略错误检查，实际使用时请注意
+
+LONG WINAPI MyUnhandledExceptionFilter(
+
+struct _EXCEPTION_POINTERS* ExceptionInfo
+
+    )
+
+{
+
+    HANDLE lhDumpFile = CreateFile(_T("DumpFile.dmp"), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS,FILE_ATTRIBUTE_NORMAL ,NULL);
+
+ 
+
+    MINIDUMP_EXCEPTION_INFORMATION loExceptionInfo;
+
+    loExceptionInfo.ExceptionPointers = ExceptionInfo;
+
+    loExceptionInfo.ThreadId = GetCurrentThreadId();
+
+    loExceptionInfo.ClientPointers = TRUE;
+
+    MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(),lhDumpFile, MiniDumpNormal, &loExceptionInfo, NULL, NULL);
+
+ 
+
+    CloseHandle(lhDumpFile);
+
+ 
+
+    return EXCEPTION_EXECUTE_HANDLER;
+
+} 
+
+void Fun2()
+
+{
+
+    __try
+
+    {
+
+       static bool b = false;
+
+       
+
+       if(!b)
+
+       {
+
+           b = true;
+
+           int *p = NULL;
+
+           *p = 0;
+
+        }
+
+       else
+
+       {
+
+           MessageBox(NULL, _T("Here"), _T(""), MB_OK);
+
+       }
+
+ 
+
+    }
+
+     __except(MyUnhandledExceptionFilter(GetExceptionInformation()))
+
+    {
+
+    }
+
+}
+
+void Fun()
+
+{
+
+    Fun2();
+
+}
+
+int main()
+
+{
+
+    Fun();
+    Fun();
+    return 1;
+}
